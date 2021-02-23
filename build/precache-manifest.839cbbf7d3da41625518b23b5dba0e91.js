637f59b5687912ebbaee4d9f58edd89470e5ee4e
amin <aminghanbari@hotmail.com>
2021-02-24 02:00:27 +0300

    build push

---
+++ build/precache-manifest.839cbbf7d3da41625518b23b5dba0e91.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "591aa428cc86ceca99ef57cc6cbc6840",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac6475588255fd938ad2",
+    "url": "/static/css/main.5a85a8d0.chunk.css"
+  },
+  {
+    "revision": "9b011ade1bee2e78c6e2",
+    "url": "/static/js/2.4770dedf.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.4770dedf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac6475588255fd938ad2",
+    "url": "/static/js/main.763cda8d.chunk.js"
+  },
+  {
+    "revision": "5958a01ab29263f7ad49",
+    "url": "/static/js/runtime-main.74d50bdc.js"
+  },
+  {
+    "revision": "217a36f841be090689787378f9416e66",
+    "url": "/static/media/avatar.217a36f8.png"
+  },
+  {
+    "revision": "f37af7173fd4ebe2a830139e6a2d079e",
+    "url": "/static/media/below.f37af717.png"
+  },
+  {
+    "revision": "16a8739859691e5a28f29c09bae8d68f",
+    "url": "/static/media/bg-banner2.16a87398.png"
+  },
+  {
+    "revision": "addc02f4dcf0e2f6161de5608c6e8081",
+    "url": "/static/media/flag.addc02f4.svg"
+  },
+  {
+    "revision": "8f7d88909a3a4260cd74404195b20f26",
+    "url": "/static/media/left.8f7d8890.svg"
+  },
+  {
+    "revision": "15009fc6e4e8765c5c4b9f0505ff8730",
+    "url": "/static/media/multi.15009fc6.svg"
+  },
+  {
+    "revision": "d60da76654315aeb6b939915d09fe5b1",
+    "url": "/static/media/overview.d60da766.svg"
+  },
+  {
+    "revision": "f85928ac04c86f7438c45c4aebd87faa",
+    "url": "/static/media/q-a.f85928ac.svg"
+  },
+  {
+    "revision": "9b11d760a851970663d52fb38c542849",
+    "url": "/static/media/question.9b11d760.png"
+  },
+  {
+    "revision": "c7787fb340a8ebe87273dd7e19cd84f3",
+    "url": "/static/media/red-flag.c7787fb3.svg"
+  },
+  {
+    "revision": "234b7a0abfa357a0b76d7d0fc3d25eb3",
+    "url": "/static/media/right.234b7a0a.svg"
+  },
+  {
+    "revision": "13459f85b410dad064caa525dfd5ba8b",
+    "url": "/static/media/single.13459f85.svg"
+  },
+  {
+    "revision": "0ccbd79ea396417d56ee33d3ed2e33d5",
+    "url": "/static/media/teacher.0ccbd79e.png"
+  }
+]);
